dbe72b66dbc45897bdf862d6b982224ca90bdd58
patricialoau <patricialoau@gmail.com>
2023-09-05 17:15:52 -0600

    Fixes component size to fit mobile viewport

--- src/components/QrCode.vue
+++ src/components/QrCode.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="qr-code">
-    <div class="bg-white qr-code-card p-5 rounded-3xl">
+    <div class="bg-white qr-code-card p-4 rounded-3xl">
       <div class="rounded overflow-hidden">
         <img class="w-full rounded-xl" src="@/assets/images/image-qr-code.png" alt="Frontend Masters QR code">
-        <div class="qr-code-card-copy px-5 py-6">
-          <h2 class="text-center mb-2">Improve your front-end skills by building projects</h2>
+        <div class="qr-code-card-copy px-4 py-5">
+          <h2 class="text-center">Improve your front-end skills by building projects</h2>
           <p class="text-center text-base pt-2">
             Scan the QR code to visit Frontend Mentor and take your coding skills to the next level
           </p>
